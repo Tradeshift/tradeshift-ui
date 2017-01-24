513a4322a45a2d0d372a30b96ec565f243b143c4
Wired Earp <wiredearp@gmail.com>
2017-01-24 16:26:24 +0100

    Screenshots list

--- docs/tasks/screenshots.js
+++ docs/tasks/screenshots.js
@@ -1,20 +1,20 @@
 module.exports = function screenshots(webdriver, driver, shoot) {
 	return [
 
+		shoot('components/screenshots/asides.html', function(done) {
+			driver.saveScreenshot('asides.png').then(done);
+		}),
+
+		shoot('components/screenshots/bars.html', function(done) {
+			driver.saveScreenshot('bars.png').then(done);
+		}),
+
 		shoot('components/screenshots/buttons.html', function(done) {
 			driver.saveScreenshot('buttons.png').then(done);
 		}),
 
 		shoot('components/screenshots/forms.html', function(done) {
 			driver.saveScreenshot('forms.png').then(done);
-		}),
-
-		shoot('components/screenshots/bars.html', function(done) {
-			driver.saveScreenshot('bars.png').then(done);
-		}),
-
-		shoot('components/screenshots/bars.html', function(done) {
-			driver.saveScreenshot('bars.png').then(done);
 		}),
 
 		shoot('components/screenshots/panels.html', function(done) {

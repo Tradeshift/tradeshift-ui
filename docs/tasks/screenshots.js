86a19613413e1323d235f2a2e94e8514187cbb6a
Wired Earp <wiredearp@gmail.com>
2017-01-25 11:09:38 +0100

    Code that we lost in the merge + folder structure

--- docs/tasks/screenshots.js
+++ docs/tasks/screenshots.js
@@ -1,23 +1,25 @@
 module.exports = function screenshots(webdriver, driver, shoot) {
 	return [
 
-		shoot('components/screenshots/asides.html', function(done) {
-			driver.saveScreenshot('asides.png').then(done);
+		shoot('screenshots/pages/asides.html', function(done) {
+			setTimeout(function waitforaside() {
+				driver.saveScreenshot('asides.png').then(done);
+			}, 2000);
 		}),
 
-		shoot('components/screenshots/bars.html', function(done) {
+		shoot('screenshots/pages/bars.html', function(done) {
 			driver.saveScreenshot('bars.png').then(done);
 		}),
 
-		shoot('components/screenshots/buttons.html', function(done) {
+		shoot('screenshots/pages/buttons.html', function(done) {
 			driver.saveScreenshot('buttons.png').then(done);
 		}),
 
-		shoot('components/screenshots/forms.html', function(done) {
+		shoot('screenshots/pages/forms.html', function(done) {
 			driver.saveScreenshot('forms.png').then(done);
 		}),
 
-		shoot('components/screenshots/panels.html', function(done) {
+		shoot('screenshots/pages/panels.html', function(done) {
 			driver.saveScreenshot('panels.png').then(done);
 		})
 

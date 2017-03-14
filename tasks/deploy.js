55960bb2fad8f4e4cd17c29405638810ec2166bf
Daniel Spitzer <dsp@tradeshift.com>
2017-03-14 17:05:44 +0100

    [S3] Upload files individually
    [S3] Added spaces to output

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,6 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const mime = require('mime');
 const s3 = require('s3');
 
 http.globalAgent.maxSockets = https.globalAgent.maxSockets = 20;
@@ -20,7 +21,7 @@ const params = {
 
 	s3Params: {
 		Bucket: 'tsresources',
-		Prefix: `prod/client/`,
+		Prefix: 'prod/client/',
 		ACL: 'public-read',
 		CacheControl: 'max-age=29030400, public',
 		ContentEncoding: 'gzip'
@@ -33,17 +34,35 @@ console.log(chalk.cyan(params.localDir));
 console.log('to');
 console.log(chalk.cyan(`s3://${params.s3Params.Bucket}/${params.s3Params.Prefix}`));
 console.log();
-var uploader = client.uploadDir(params);
-uploader.on('error', (err) => {
-	console.error('S3 uploading ' + chalk.red('failed') + '!');
-	console.error(JSON.stringify(err, null, 4));
-});
-uploader.on('progress', () => {
-	if (uploader.progressTotal > 0) {
-		const percentage = (uploader.progressAmount / uploader.progressTotal) * 100;
-		console.log(chalk.magenta(`${percentage || 0}%`));
+
+fs.readdir(params.localDir, (readErr, items) => {
+	if (readErr) {
+		console.error(JSON.stringify(readErr, null, 4));
 	}
-});
-uploader.on('end', () => {
-	console.log('Deployed ' + chalk.green('successfully') + ' to S3!');
+	items.forEach((file) => {
+		const fileParams = Object.assign({}, params, {
+			localFile: `${params.localDir}/${file}`,
+			s3Params: Object.assign({}, params.s3Params, {
+				ContentType: mime.lookup(file),
+				Key: `${params.s3Params.Prefix}${file}`
+			})
+		});
+		delete fileParams.localDir;
+		delete fileParams.s3Params.Prefix;
+		const uploader = client.uploadFile(fileParams);
+
+		console.log(chalk.magenta(`[${file}] `) + 'Uploading...');
+		uploader.on('error', (uploadErr) => {
+			console.error(chalk.magenta(`[${file}] `) + 'S3 uploading ' + chalk.red('failed') + '\n' + JSON.stringify(uploadErr, null, 4));
+		});
+		uploader.on('progress', () => {
+			if (uploader.progressTotal > 0) {
+				const percentage = (uploader.progressAmount / uploader.progressTotal) * 100;
+				console.log(chalk.magenta(`[${file}] `) + chalk.magenta(`${percentage || 0}%`));
+			}
+		});
+		uploader.on('end', () => {
+			console.log(chalk.magenta(`[${file}] `) + `Uploaded ` + chalk.green('successfully') + ' to S3!');
+		});
+	});
 });

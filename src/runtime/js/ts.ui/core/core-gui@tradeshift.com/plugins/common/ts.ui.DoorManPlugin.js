14555a3d426f039b3b1ec6c0049856c83ff639a5
Wired Earp <wiredearp@gmail.com>
2017-05-31 16:41:27 +0200

    DoorManPlugin continues

--- src/runtime/js/ts.ui/core/core-gui@tradeshift.com/plugins/common/ts.ui.DoorManPlugin.js
+++ src/runtime/js/ts.ui/core/core-gui@tradeshift.com/plugins/common/ts.ui.DoorManPlugin.js
@@ -6,7 +6,7 @@
  */
 ts.ui.DoorManPlugin = (function(Type) {
 	// custom dom events (for public consumption)
-	// TODO: These are now used in SideBars and Modals and should be renamed
+	// TODO: These are now used in SideBars and Modals and should be renamed!!!!!!
 	var domevent = {
 		WILLOPEN: ts.ui.EVENT_ASIDE_WILL_OPEN,
 		DIDOPEN: ts.ui.EVENT_ASIDE_DID_OPEN,
@@ -17,24 +17,20 @@ ts.ui.DoorManPlugin = (function(Type) {
 	return ts.ui.Plugin.extend(
 		{
 			/**
-		 * TODO: Only in debug mode
-		 */
+			 * In debug mode, make sure that the host (spirit) is set up correctly.
+			 */
 			onconstruct: function() {
 				this.super.onconstruct();
-				var apis = 'isOpen onopen onopened onclose onclosed $onopen $onclose';
-				var host = this.spirit;
-				apis.split(' ').forEach(function(api) {
-					if (host[api] === undefined) {
-						throw new Error('Missing interface "' + api + '"');
-					}
-				});
+				if (gui.debug) {
+					this._confirminterface();
+				}
 			},
 
 			/**
-		 * Open AND close the aside (setup to support HTML `data-ts.open="true|false"`)
-		 * @param @optional {boolean} opt_open Omit to simply open.
-		 * @returns {boolean} True if allowed to open
-		 */
+			 * Open AND close the aside (setup to support HTML `data-ts.open="true|false"`)
+			 * @param @optional {boolean} opt_open Omit to simply open.
+			 * @returns {boolean} True if allowed to open
+			 */
 			open: function(opt_open) {
 				var spirit = this.spirit;
 				if (!Type.isBoolean(opt_open)) {
@@ -55,21 +51,22 @@ ts.ui.DoorManPlugin = (function(Type) {
 			},
 
 			/**
-		 *
-		 */
+			 * The host (spirit) should make sure to call this when fully opened.
+			 * This likely dependes on some CSS transition or animation or such.
+			 */
 			didopen: function() {
 				this._execute('onopened');
-				this.spirit.event.dispatch(domevent.DIDCLOSE, {
+				this.spirit.event.dispatch(domevent.DIDOPEN, {
 					bubbles: true
 				});
 			},
 
 			/**
-		 *
-		 */
+			 * The host (spirit) should make sure to call this when fully closed.
+			 */
 			didclose: function() {
 				this._execute('onclosed');
-				this.spirit.event.dispatch(domevent.DIDOPEN, {
+				this.spirit.event.dispatch(domevent.DIDCLOSE, {
 					bubbles: true
 				});
 			},
@@ -77,10 +74,10 @@ ts.ui.DoorManPlugin = (function(Type) {
 			// Private .................................................................
 
 			/**
-		 * Fire custom DOM event and return whether or not this was preventDefaulted.
-		 * @param {boolean} open
-		 * @returns {boolean}
-		 */
+			 * Fire custom DOM event and return whether or not this was preventDefaulted.
+			 * @param {boolean} open
+			 * @returns {boolean}
+			 */
 			_shouldattempt: function(open) {
 				var events = this.spirit.event;
 				if (open !== this.spirit.isOpen) {
@@ -93,11 +90,11 @@ ts.ui.DoorManPlugin = (function(Type) {
 			},
 
 			/**
-		 * Execute callback configured via HTML attribute or via JS property.
-		 * The 'this' keyword points to the element or the spirit respectively.
-		 * @type {String|function}
-		 * @returns {boolean}
-		 */
+			 * Execute callback configured via HTML attribute or via JS property.
+			 * The 'this' keyword points to the element or the spirit respectively.
+			 * @type {String|function}
+			 * @returns {boolean}
+			 */
 			_execute: function(callback) {
 				var spirit = this.spirit;
 				if ((callback = spirit[callback])) {
@@ -112,10 +109,10 @@ ts.ui.DoorManPlugin = (function(Type) {
 			},
 
 			/**
-		 * Opening.
-		 * @param {boolean} animated (TODO)
-		 * @returns {boolean}
-		 */
+			 * Opening.
+			 * @param {boolean} animated (TODO)
+			 * @returns {boolean}
+			 */
 			_maybeopen: function(animated) {
 				var ok = this._execute('onopen') !== false;
 				if (ok) {
@@ -127,10 +124,10 @@ ts.ui.DoorManPlugin = (function(Type) {
 			},
 
 			/**
-		 * Closing.
-		 * @param {boolean} animated (TODO)
-		 * @returns {boolean}
-		 */
+			 * Closing.
+			 * @param {boolean} animated (TODO)
+			 * @returns {boolean}
+			 */
 			_maybeclose: function(animated) {
 				var ok = this._execute('onclose') !== false;
 				if (ok) {
@@ -139,14 +136,28 @@ ts.ui.DoorManPlugin = (function(Type) {
 					this.spirit.$onclose(animated);
 				}
 				return ok;
+			},
+
+			/**
+			 * Confirm methods implemented or at least stubbed with `null`.
+			 */
+			_confirminterface: function() {
+				var apis = 'isOpen onopen onopened onclose onclosed $onopen $onclose';
+				var host = this.spirit;
+				apis.split(' ').forEach(function(api) {
+					if (host[api] === undefined) {
+						throw new Error('Missing interface "' + api + '"');
+					}
+				});
 			}
 		},
 		{
 			// Static ...............................................................
 
 			/**
-		 * 
-		 */
+			 * This plugins gets newed up automatically along with the spirit.
+			 * @type {boolean}
+			 */
 			lazy: false
 		}
 	);

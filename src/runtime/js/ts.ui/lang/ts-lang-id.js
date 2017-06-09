cc02a1a24ab7c3d52b9d7011af67a2b8a3b8b466
Wired Earp <wiredearp@gmail.com>
2017-06-09 13:04:16 +0200

    Fixes #290 - Indonesian, Malaysian, Romanian and Traditional Chinese

---
+++ src/runtime/js/ts.ui/lang/ts-lang-id.js
@@ -0,0 +1,36 @@
+if (window.ts && ts.ui && ts.ui.DatePicker) {
+	ts.ui.DatePicker.localize({
+		firstDay: 0,
+		monthNames: [
+			'Januari',
+			'Februari',
+			'Maret',
+			'April',
+			'Mei',
+			'Juni',
+			'Juli',
+			'Agustus',
+			'September',
+			'Oktober',
+			'Nopember',
+			'Desember'
+		],
+		monthNamesShort: [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'Mei',
+			'Jun',
+			'Jul',
+			'Agus',
+			'Sep',
+			'Okt',
+			'Nop',
+			'Des'
+		],
+		dayNames: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+		dayNamesShort: ['Min', 'Sen', 'Sel', 'Rab', 'kam', 'Jum', 'Sab'],
+		dayNamesMin: ['Mg', 'Sn', 'Sl', 'Rb', 'Km', 'jm', 'Sb']
+	});
+}
---
+++ src/runtime/js/ts.ui/lang/ts-lang-ms.js
@@ -0,0 +1,36 @@
+if (window.ts && ts.ui && ts.ui.DatePicker) {
+	ts.ui.DatePicker.localize({
+		firstDay: 0,
+		monthNames: [
+			'Januari',
+			'Februari',
+			'Mac',
+			'April',
+			'Mei',
+			'Jun',
+			'Julai',
+			'Ogos',
+			'September',
+			'Oktober',
+			'November',
+			'Disember'
+		],
+		monthNamesShort: [
+			'Jan',
+			'Feb',
+			'Mac',
+			'Apr',
+			'Mei',
+			'Jun',
+			'Jul',
+			'Ogo',
+			'Sep',
+			'Okt',
+			'Nov',
+			'Dis'
+		],
+		dayNames: ['Ahad', 'Isnin', 'Selasa', 'Rabu', 'Khamis', 'Jumaat', 'Sabtu'],
+		dayNamesShort: ['Aha', 'Isn', 'Sel', 'Rab', 'kha', 'Jum', 'Sab'],
+		dayNamesMin: ['Ah', 'Is', 'Se', 'Ra', 'Kh', 'Ju', 'Sa']
+	});
+}

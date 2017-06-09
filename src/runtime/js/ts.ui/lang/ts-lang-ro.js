84c2cb15fda5b6ace8e5de5730dec47b46240842
Wired Earp <wiredearp@gmail.com>
2017-06-09 13:27:46 +0200

    Indonesian, Malaysian, Romanian and Traditional Chinese (#293)
    * Fixes #290 - Indonesian, Malaysian, Romanian and Traditional Chinese
    * Optimized Mandarin

---
+++ src/runtime/js/ts.ui/lang/ts-lang-ro.js
@@ -0,0 +1,36 @@
+if (window.ts && ts.ui && ts.ui.DatePicker) {
+	ts.ui.DatePicker.localize({
+		firstDay: 1,
+		monthNames: [
+			'Ianuarie',
+			'Februarie',
+			'Martie',
+			'Aprilie',
+			'Mai',
+			'Iunie',
+			'Iulie',
+			'August',
+			'Septembrie',
+			'Octombrie',
+			'Noiembrie',
+			'Decembrie'
+		],
+		monthNamesShort: [
+			'Ian',
+			'Feb',
+			'Mar',
+			'Apr',
+			'Mai',
+			'Iun',
+			'Iul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec'
+		],
+		dayNames: ['Duminică', 'Luni', 'Marţi', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'],
+		dayNamesShort: ['Dum', 'Lun', 'Mar', 'Mie', 'Joi', 'Vin', 'Sâm'],
+		dayNamesMin: ['Du', 'Lu', 'Ma', 'Mi', 'Jo', 'Vi', 'Sâ']
+	});
+}

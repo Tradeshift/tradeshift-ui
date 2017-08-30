b338ba8d9b320189ca05c11063f1e9168e9ad6a7
Daniel Spitzer <dsp@tradeshift.com>
2017-08-30 14:07:39 +0200

    [Table] Updated tests
    Fix for the new `checkbox` classes

--- spec/runtime/edbml/functions/ts.ui.tablemenu.edbml.spec.js
+++ spec/runtime/edbml/functions/ts.ui.tablemenu.edbml.spec.js
@@ -6,7 +6,7 @@ describe('ts.ui.tablemenu.edbml', function likethis() {
 	it('should contain ts-table-menu', function() {
 		var table = new ts.ui.TableModel();
 		expect(gethtml(table)).toContain('ts-table-menu');
-		expect(gethtml(table)).toContain('<th class="ts-table-checkbox">');
+		expect(gethtml(table)).toMatch(/<th class=\"ts-table-checkbox([^"]*)"/);
 		expect(gethtml(table)).toContain('<button data-action="selection-menu"');
 	});
 
@@ -27,14 +27,14 @@ describe('ts.ui.tablemenu.edbml', function likethis() {
 	it('should contain ts-icon-checkboxon', function() {
 		var table = new ts.ui.TableModel();
 		spyOn(table, 'isVisibleRowSelected').and.returnValue(true);
-		expect(gethtml(table)).toContain('class="ts-icon-checkboxon"');
-		expect(gethtml(table)).not.toContain('class="ts-icon-checkbox"');
+		expect(gethtml(table)).toContain('class="ts-icon-checkboxon');
+		expect(gethtml(table)).not.toContain('class="ts-icon-checkbox');
 	});
 
 	it('should contain ts-icon-checkbox', function() {
 		var table = new ts.ui.TableModel();
 		spyOn(table, 'isVisibleRowSelected').and.returnValue(false);
-		expect(gethtml(table)).toContain('class="ts-icon-checkbox"');
-		expect(gethtml(table)).not.toContain('class="ts-icon-checkboxon"');
+		expect(gethtml(table)).toContain('class="ts-icon-checkbox');
+		expect(gethtml(table)).not.toContain('class="ts-icon-checkboxon');
 	});
 });

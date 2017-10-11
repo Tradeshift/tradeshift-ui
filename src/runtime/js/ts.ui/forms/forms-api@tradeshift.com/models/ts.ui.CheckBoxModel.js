2607da16d1397854aa28a2b31898856cd9b3844c
Bo Mouridsen <bmo@tradeshift.com>
2017-10-11 14:10:39 +0200

    Revert "indent fix"
    This reverts commit 0c77cc19049d1c0f125d23106312bdaba237c25d.

--- src/runtime/js/ts.ui/forms/forms-api@tradeshift.com/models/ts.ui.CheckBoxModel.js
+++ src/runtime/js/ts.ui/forms/forms-api@tradeshift.com/models/ts.ui.CheckBoxModel.js
@@ -4,67 +4,67 @@
  * @using {gui.Combo.chained}
  */
 ts.ui.CheckBoxModel = (function using(chained) {
-	return ts.ui.Model.extend({
-		/**
+  return ts.ui.Model.extend({
+    /**
 		 * Most commonly used as a tooltip.
 		 * @type {string}
 		 */
-		info: '',
+    info: "",
 
-		/**
+    /**
 		 * Checked?
 		 * @type {boolean}
 		 */
-		checked: false,
+    checked: false,
 
-		/**
+    /**
 		 * Indeterminate?
 		 * @type {boolean}
 		 */
-		indeterminate: false,
+    indeterminate: false,
 
-		/**
+    /**
 		 * Visible?
 		 * @type {boolean}
 		 */
-		visible: true,
+    visible: true,
 
-		/**
+    /**
 		 * Open for implementation: Called when clicked (not handled in the model!).
 		 * @type {function}
 		 */
-		onclick: null,
+    onclick: null,
 
-		/**
+    /**
 		 * Check that checkbox.
 		 * @returns {this}
 		 */
-		check: chained(function() {
-			this.checked = true;
-		}),
+    check: chained(function() {
+      this.checked = true;
+    }),
 
-		/**
+    /**
 		 * Uncheck the checkbox.
 		 * @returns {this}
 		 */
-		uncheck: chained(function() {
-			this.checked = false;
-		}),
+    uncheck: chained(function() {
+      this.checked = false;
+    }),
 
-		/**
+    /**
 		 * Hide the checkbox.
 		 * @returns {this}
 		 */
-		hide: chained(function() {
-			this.visible = false;
-		}),
+    hide: chained(function() {
+      this.visible = false;
+    }),
 
-		/**
+    /**
 		 * Show the checkbox.
 		 * @returns {this}
 		 */
-		show: chained(function() {
-			this.visible = true;
-		})
-	});
+    show: chained(function() {
+      this.visible = true;
+    })
+  });
 })(gui.Combo.chained);

360f976684d2638ca4a2682df7ce40dd1854de4b
Wired Earp <wiredearp@gmail.com>
2017-12-08 13:38:36 +0100

    More docs

--- docs/src/js/dox-gui@tradeshift.com/spirits/chrome/ts.dox.SideBarSpirit.js
+++ docs/src/js/dox-gui@tradeshift.com/spirits/chrome/ts.dox.SideBarSpirit.js
@@ -12,6 +12,15 @@ ts.dox.SideBarSpirit = ts.ui.SideBarSpirit.extend({
 	},
 
 	/**
+	 * Apparently we need to dispatch this action for the chrome to close menu. 
+	 * This method is triggered by the closebutton, which the Chrome has enabled.
+	 */
+	close: function() {
+		this.super.close();
+		this.action.dispatch('action-close-menu');
+	},
+
+	/**
 	 * This particular sidebar should just have a regular toolbar 
 	 * header (because we like how the Search looks in that one).
 	 * @returns {ts.ui.ToolBarSpirit}

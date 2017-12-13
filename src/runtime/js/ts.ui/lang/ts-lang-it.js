7f76a2df970fb48a1ea1e8285d7f49f3f2b9afc5
Wired Earp <wiredearp@gmail.com>
2017-12-13 13:55:50 +0100

    Cleanup translations
    No need to guard against load order no more.

--- src/runtime/js/ts.ui/lang/ts-lang-it.js
+++ src/runtime/js/ts.ui/lang/ts-lang-it.js
@@ -1,36 +1,34 @@
-if (window.ts && ts.ui && ts.ui.DatePicker) {
-	ts.ui.DatePicker.localize({
-		firstDay: 1,
-		monthNames: [
-			'Gennaio',
-			'Febbraio',
-			'Marzo',
-			'Aprile',
-			'Maggio',
-			'Giugno',
-			'Luglio',
-			'Agosto',
-			'Settembre',
-			'Ottobre',
-			'Novembre',
-			'Dicembre'
-		],
-		monthNamesShort: [
-			'Gen',
-			'Feb',
-			'Mar',
-			'Apr',
-			'Mag',
-			'Giu',
-			'Lug',
-			'Ago',
-			'Set',
-			'Ott',
-			'Nov',
-			'Dic'
-		],
-		dayNames: ['Domenica', 'Lunedi', 'Martedì', 'Mercoledì', 'Gioverdi', 'Venerdì', 'Sabato'],
-		dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
-		dayNamesMin: ['Do', 'Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa']
-	});
-}
+ts.ui.DatePicker.localize({
+	firstDay: 1,
+	monthNames: [
+		'Gennaio',
+		'Febbraio',
+		'Marzo',
+		'Aprile',
+		'Maggio',
+		'Giugno',
+		'Luglio',
+		'Agosto',
+		'Settembre',
+		'Ottobre',
+		'Novembre',
+		'Dicembre'
+	],
+	monthNamesShort: [
+		'Gen',
+		'Feb',
+		'Mar',
+		'Apr',
+		'Mag',
+		'Giu',
+		'Lug',
+		'Ago',
+		'Set',
+		'Ott',
+		'Nov',
+		'Dic'
+	],
+	dayNames: ['Domenica', 'Lunedi', 'Martedì', 'Mercoledì', 'Gioverdi', 'Venerdì', 'Sabato'],
+	dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
+	dayNamesMin: ['Do', 'Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa']
+});
--- src/runtime/js/ts.ui/lang/ts-lang-nl.js
+++ src/runtime/js/ts.ui/lang/ts-lang-nl.js
@@ -1,36 +1,34 @@
-if (window.ts && ts.ui && ts.ui.DatePicker) {
-	ts.ui.DatePicker.localize({
-		firstDay: 1,
-		monthNames: [
-			'Januari',
-			'Februari',
-			'Maart',
-			'April',
-			'Mei',
-			'Juni',
-			'Juli',
-			'Augustus',
-			'September',
-			'Oktober',
-			'November',
-			'December'
-		],
-		monthNamesShort: [
-			'Jan',
-			'Feb',
-			'Maa',
-			'Apr',
-			'Mei',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Okt',
-			'Nov',
-			'Dec'
-		],
-		dayNames: ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'],
-		dayNamesShort: ['Zon', 'Maa', 'Din', 'Woe', 'Don', 'Vri', 'Zat'],
-		dayNamesMin: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']
-	});
-}
+ts.ui.DatePicker.localize({
+	firstDay: 1,
+	monthNames: [
+		'Januari',
+		'Februari',
+		'Maart',
+		'April',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Augustus',
+		'September',
+		'Oktober',
+		'November',
+		'December'
+	],
+	monthNamesShort: [
+		'Jan',
+		'Feb',
+		'Maa',
+		'Apr',
+		'Mei',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Okt',
+		'Nov',
+		'Dec'
+	],
+	dayNames: ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'],
+	dayNamesShort: ['Zon', 'Maa', 'Din', 'Woe', 'Don', 'Vri', 'Zat'],
+	dayNamesMin: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']
+});

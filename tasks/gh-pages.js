f0d08b8c78d3b0df5352bcff00044aa01e9643c0
Wired Earp <wiredearp@gmail.com>
2018-01-10 15:52:03 +0100

    Fix sort order

--- tasks/gh-pages.js
+++ tasks/gh-pages.js
@@ -108,7 +108,7 @@ function updateversions(where, source, target) {
 	pkg.versions = pkg.versions
 		.map(v => (v === source ? target : v))
 		.concat(source === ZERO ? [target] : [])
-		.sort(semv.gt);
+		.sort(semv.lt);
 	setpackage('gh-pages/package.json', pkg);
 	return pkg.versions;
 }

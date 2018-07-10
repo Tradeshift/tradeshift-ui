b4bbfa3b81f500708a8108378c58c4e2942400c5
Mihnea <mihnea.zamfir@tradeshift.com>
2018-07-10 15:01:16 +0200

    Improvements to docs page

--- docs/dist/appendix/colors/index.js
+++ docs/dist/appendix/colors/index.js
@@ -2,54 +2,7 @@
  * The "colors.txt" file can just be copy-pasted from the LESS file.
  * We will parse this file into a JSON array that can be rendered.
  */
-ts.ui.ready(function rendercolors() {
-	function serious(part) {
-		return part.includes('@') && part.includes(':');
-	}
-
-	function defs(part) {
-		var cut = part.split(':');
-		var def = cut[0].trim();
-		var val = cut[1].trim();
-		return `\t\t` + quote(def.slice(def.indexOf('@'))) + ': ' + quote(val);
-	}
-
-	function object(txt) {
-		return '\t{\n' + txt + '\n\t}';
-	}
-
-	function array(txt) {
-		return '[\n' + txt + '\n]';
-	}
-
-	function quote(txt) {
-		return '"' + txt + '"';
-	}
-
-	function parseone(css) {
-		return css
-			.split(';')
-			.filter(serious)
-			.map(defs)
-			.join(',\n');
-	}
-
-	function parseall(css) {
-		return css
-			.split('//')
-			.filter(serious)
-			.map(function(section) {
-				return object(parseone(section));
-			})
-			.join(',\n');
-	}
-
-	$.get('colors.txt', function(css) {
-		ts.ui.get('#doc-colors-demo', function(spirit) {
-			spirit.script.run(JSON.parse(array(parseall(css))));
-		});
-	});
-});
+ts.ui.ready(function rendercolors() {});
 
 /*
  * TODO: Also display RGB and HEX codes!
@@ -68,7 +21,7 @@ function hslToRgb(h, s, l) {
 	return [
 		Math.max(0, Math.min(Math.round(r * 255), 255)),
 		Math.max(0, Math.min(Math.round(g * 255), 255)),
-		Math.max(0, Math.min(Math.round(b * 255), 255)) 
+		Math.max(0, Math.min(Math.round(b * 255), 255))
 	];
 }
 

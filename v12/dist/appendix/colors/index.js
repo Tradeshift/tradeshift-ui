e56fdc4fec92aaa9e3f1f36bd51854304784efdf
Daniel Spitzer <dsp@tradeshift.com>
2019-01-25 16:16:11 +0100

    Update docs for v12.0.0

--- v12/dist/appendix/colors/index.js
+++ v12/dist/appendix/colors/index.js
@@ -11,7 +11,12 @@ ts.ui.ready(function rendercolors() {
 		var cut = part.split(':');
 		var def = cut[0].trim();
 		var val = cut[1].trim();
-		return `\t\t` + quote(def.slice(def.indexOf('@'))) + ': ' + quote(val);
+		var matches = val.match(/\d+/g);
+		matches = matches.map(function(color) {
+			return +color;
+		});
+		var rgb = hslToHex(matches[0], matches[1], matches[2]);
+		return '\t\t' + quote(def.slice(def.indexOf('@'))) + ': ' + quote(rgb);
 	}
 
 	function object(txt) {
@@ -44,40 +49,60 @@ ts.ui.ready(function rendercolors() {
 			.join(',\n');
 	}
 
+	function hslToHex(h, s, l) {
+		h /= 360;
+		s /= 100;
+		l /= 100;
+		var r, g, b;
+		if (s === 0) {
+			r = g = b = l; // achromatic
+		} else {
+			var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			var p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1 / 3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1 / 3);
+		}
+		return '#' + toHex(r) + toHex(g) + toHex(b);
+	}
+
+	function hue2rgb(p, q, t) {
+		if (t < 0) t += 1;
+		if (t > 1) t -= 1;
+		if (t < 1 / 6) return p + (q - p) * 6 * t;
+		if (t < 1 / 2) return q;
+		if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+		return p;
+	}
+
+	function toHex(x) {
+		var hex = Math.round(x * 255).toString(16);
+		return hex.length === 1 ? '0' + hex : hex;
+	}
+
+	function copyColor() {
+		var colorChips = document.querySelectorAll('.color-chip');
+
+		colorChips.forEach(function(colorChip) {
+			colorChip.addEventListener('click', function(e) {
+				var input = document.createElement('input');
+				input.id = 'copyInput';
+				input.value = e.target.querySelector('.color-code').textContent;
+				document.body.appendChild(input);
+
+				var copyText = document.getElementById('copyInput');
+				copyText.select();
+				document.execCommand('copy');
+				ts.ui.Notification.success('Color ' + copyText.value + ' copied to your clipboard.');
+				document.body.removeChild(input);
+			});
+		});
+	}
+
 	$.get('colors.txt', function(css) {
 		ts.ui.get('#doc-colors-demo', function(spirit) {
 			spirit.script.run(JSON.parse(array(parseall(css))));
+			copyColor();
 		});
 	});
 });
-
-/*
- * TODO: Also display RGB and HEX codes!
- *
-function hslToRgb(h, s, l) {
-	var r, g, b;
-	if (s === 0) {
-		r = g = b = l; // achromatic
-	} else {
-		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-		var p = 2 * l - q;
-		r = hue2rgb(p, q, h + 1 / 3);
-		g = hue2rgb(p, q, h);
-		b = hue2rgb(p, q, h - 1 / 3);
-	}
-	return [
-		Math.max(0, Math.min(Math.round(r * 255), 255)),
-		Math.max(0, Math.min(Math.round(g * 255), 255)),
-		Math.max(0, Math.min(Math.round(b * 255), 255)) 
-	];
-}
-
-function hue2rgb(p, q, t) {
-	if (t < 0) t += 1;
-	if (t > 1) t -= 1;
-	if (t < 1 / 6) return p + (q - p) * 6 * t;
-	if (t < 1 / 2) return q;
-	if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-	return p;
-}
-*/
